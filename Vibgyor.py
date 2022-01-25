b8336d6751712dcd4793b0c9bc49b5ee3dac8c76
Mxtylish007 <91541128+Mxtylish007@users.noreply.github.com>
2022-01-25 18:55:22 +0530

    Create Vibgyor.py

---
+++ Vibgyor.py
@@ -0,0 +1,29 @@
+from turtle import *
+bgcolor("black")
+speed(0)
+hideturtle()
+for i in range(25):
+	color("red")
+	circle(10+i)
+	range(10)
+for i in range(25):
+	color("orange")
+	circle(i+25+10)
+	range(0)
+for i in range(20):
+	color("yellow")
+	circle(i+50+10)
+for i in range(20):
+	color("green")
+	circle(i+50+20+10)
+for i in range(15):
+	color("blue")
+	circle(i+50+40+10)
+for i in range(15):
+	color("indigo")
+	circle(i+50+55+10)
+for i in range(10):
+	color("violet")
+	circle(i+50+70+10)
+	
+done()
