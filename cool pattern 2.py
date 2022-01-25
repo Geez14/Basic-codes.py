4c9e7275c84f6b511d9959c4ac827d308b8a9e07
Mxtylish007 <91541128+Mxtylish007@users.noreply.github.com>
2022-01-25 18:55:01 +0530

    Add files via upload

---
+++ cool pattern 2.py
@@ -0,0 +1,42 @@
+from turtle import *
+
+
+bgcolor("black")
+speed(0)
+hideturtle()
+for i in range(8):
+    color("red")
+    circle(i)
+    right(0)
+    forward(1)
+for i in range(10):
+    color("orange")
+    circle(i)
+    right(1)
+    forward(2)
+for i in range(12):
+    color("yellow")
+    circle(i)
+    right(2)
+    forward(3)
+for i in range(14):
+    color("green")
+    circle(i)
+    right(3)
+    forward(4)
+for i in range(16):
+    color("blue")
+    circle(i)
+    right(4)
+    forward(5)
+for i in range(18):
+    color("indigo")
+    circle(i)
+    right(5)
+    forward(6)
+for i in range(18):
+    color("violet")
+    circle(i)
+    right(6)
+    forward(7)
+done()
